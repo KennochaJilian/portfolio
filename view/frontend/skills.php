2c109ec6805e852b63ccdcd69a4197908df03506
Aranxa.Codina <tassa780@gmail.com>
2019-12-22 13:02:15 +0100

    Génération des tableaux de compétences

--- view/frontend/skills.php
+++ view/frontend/skills.php
@@ -1,7 +1,52 @@
-<section class="row d-flex flex-column align-items-center" id="skills">
-<div class="mb-5">
-        <h2 class="category"> Mes Compétences </h2>
-        <hr class="line">
+<?php 
+    $skillsTable=[
+        [
+        "title" => "Front-End",
+        "imgSRC" =>"Test", 
+        ],
+
+        [
+            "title" => "Back-End",
+            "imgSRC" => "Test", 
+        ],
+
+        [
+            "title" => "Administration",
+            "imgSRC" => "Test", 
+        ],
+
+        [
+            "title" => "Divers",
+            "imgSRC" => "Test", 
+        ],
+    ]
+
+
+?>
+<section id="skills">
+    <div class=" row d-flex flex-column align-items-center">
+        <div class="mb-5">
+            <h2 class="category"> Mes Compétences </h2>
+            <hr class="line">
+        </div>
     </div>
+    <aside class="row">
+       
+
+            <?php 
+            foreach($skillsTable as $skill){ ?>
+            <div class="col-md-2 skillsTable">
+                <div>
+                    <h3> <?= $skill['title']?></h3>
+                </div>
+                <div class="imgSkills"> <?=$skill['imgSRC']?></div>
+            </div>
+            <?php
+            }
+        ?>
+
+     
+
+    </aside>
 
 </section>
