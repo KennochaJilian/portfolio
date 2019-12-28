e5d09dd24223fe10811357c80a74c88766d26dfe
Aranxa.Codina <tassa780@gmail.com>
2019-12-28 14:52:58 +0100

    Mise en forme de la page contact

--- view/frontend/contactMe.php
+++ view/frontend/contactMe.php
@@ -6,28 +6,28 @@
 
     <div class="col-md-12">
         <p class="text-center"> Vous désirez plus d'information me concernant, ou vous souhaitez échanger avec moi?
-            Envoyez moi un message et je je vous répondrai avec plaisir </p>
+            Envoyez moi un message et je je vous répondrai avec plaisir ! </p>
     </div>
 
     <div class="col-md-12">
         <form action="" method="post">
-            <div class="row">
-                <div class="col-lg-4 offset-md-2">
+            <div class="row mb-3">
+                <div class="col-lg-4 offset-lg-2 col-sm-10">
 
-                    <div class="form-group">
-                        <input required type="text" name="name" class="form-control" id="inputname"
+                    <div class="form-group field">
+                        <input required type="text" name="name" class="form-control field" id="inputname"
                             placeholder="Votre nom" value="">
                     </div>
 
 
                     <div class="form-group">
-                        <input required type="text" name="email" class="form-control" id="inputemail"
+                        <input required type="text" name="email" class="form-control field" id="inputemail"
                             placeholder="Votre mail" value="">
                     </div>
 
 
                     <div class="form-group">
-                        <select class="form-control" id="question">
+                        <select class="form-control field" id="question">
                             <option>Une question?</option>
                             <option>Une suggestion?</option>
                         </select>
@@ -35,17 +35,28 @@
 
                 </div>
 
-                <div class="col-lg-4">
-                    <div class="form-group">
-                        <textarea id="contact_form_message" name="message" required="required" placeholder="Votre message" rows="6" class="form-control"></textarea>
+                <div class="col-lg-4 col-sm-10">
+                    <div class="form-group field">
+                        <textarea id="contact_form_message" name="message" required="required"
+                            placeholder="Votre message" rows="5" class="form-control field"></textarea>
                     </div>
                 </div>
-
-                <div class="col-lg-12">
-                    <button type="submit" class="btnWelcome">Envoyer</button>
-                </div>                
+            </div>
+            <div class="d-flex justify-content-center mb-4">
+                <div>
+                    <button type="submit" class="btnContact">Envoyer</button>
+                </div>
             </div>
         </form>
     </div>
 
+
+    <div class="col-lg-12 mb-5">
+        <div class="d-flex justify-content-center">
+            <div class="icone mx-2"> <a href=""> <i class="  fab fa-linkedin-in"></i> </a> </div>
+            <div class="icone mx-2"> <a href=""> <i class="  fab fa-github"></i> </a> </div>
+            <div class="icone mx-2"> <a href=""> <i class=" fab fa-twitter"></i> </a> </div>
+            <div class="icone mx-2"> <a href=""> <i class=" far fa-envelope"></i> </a> </div>
+        </div>
+    </div>
 </section>
--- view/frontend/header.php
+++ view/frontend/header.php
@@ -5,7 +5,8 @@
         <title><?= $title?></title>
         <link href="public/css/style.css" rel="stylesheet" /> 
         <link href="https://fonts.googleapis.com/css?family=ABeeZee&display=swap" rel="stylesheet">
-	    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" />
+        <script src="https://kit.fontawesome.com/1a376cf290.js"></script>
     </head>
         
     <body>
