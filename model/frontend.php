c0ff957fb97afa207ce952f419835147402f9f97
Aranxa.Codina <tassa780@gmail.com>
2020-01-01 15:26:02 +0100

    Ajout fichier config.php

--- model/frontend.php
+++ model/frontend.php
@@ -1,8 +1,10 @@
 <?php
+
 function dbConnect(){
+    require ('config.php'); 
     try
     {
-        $db = new PDO('mysql:host=localhost;dbname=portfolio;charset=utf8', 'root', 'Aranx@_1993');
+        $db = new PDO('mysql:host=localhost;dbname=portfolio;charset=utf8', 'root', $mdpDataBase);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         return $db;
